77b97172831960b90f511d729d2bb5323e54018c
Rishik <142647228+rishikpogu@users.noreply.github.com>
2024-05-31 17:00:23 +0530

    solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...b) {
+    // let newArr = []
+    for (let i = 0; i < array.length; i++) {
+        for (let j of b) {
+            if (array[i] === j) {
+                array.splice(i, 1)
+                i--
+                break
+            }
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
