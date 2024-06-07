02b4382e7f14b29c070943e48c58fee01b94154d
Rishik <142647228+rishikpogu@users.noreply.github.com>
2024-06-07 23:03:48 +0530

    solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 1 || num == 2) {
+        return 1
+    }
+    else if (num == 0) {
+        return 0
+    }
+    else if (num < 0) {
+        return "OOPS"
+    }
+    let a = 1
+    let b = 1
+    let c
+    for (let i = 3; i <= num; i++) {
+        c = a + b
+        a = b
+        b = c
+    }
+    return c;
 };
 
 // Do not edit below this line
