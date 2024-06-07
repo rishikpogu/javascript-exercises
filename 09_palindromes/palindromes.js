5564bb7a1e399dea58bd6d0f8e9ec3c08e703be5
Rishik <142647228+rishikpogu@users.noreply.github.com>
2024-06-07 22:48:22 +0530

    solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const inp = 'abcdefghijklmnopqrstuvwxyz01234566789';
+    const upstr = str.toLowerCase().split('').filter((c) => inp.includes(c)).join('');
 
+    const revstr = upstr.split('').reverse().join('');
+    return upstr === revstr;
 };
 
 // Do not edit below this line
